5b25f065bc35aebc633970a0cb0b50b6311d03cc
Winkin Yu <genehit@163.com>
2025-07-16 03:11:18 +0800

    delete the exercises under hands_on

--- hands_on/sandbox/run_exp.sh
+++
@@ -1,4 +0,0 @@
-
-# train and play
-python hands_on/exercise2_dqn/dqn_main.py --config hands_on/exercise2_dqn/obs_1d_config.json
-python hands_on/exercise2_dqn/dqn_main.py --config hands_on/exercise2_dqn/obs_img_config.json
